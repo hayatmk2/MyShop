f90e8bd8e895ebe049a214ced901ab57ef0aeac5
Hayat <hayatmk4@gmail.com>
2022-09-01 08:57:05 -0700

    react bootstrap and bootswatch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
+import {Container} from 'react-bootstrap';
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 // arrow functions
 const App = () => {
   return (
     <>
+    <Header />
+    <main>
+      <Container>
       <h1>Welcome to MyShop!</h1>
+      </Container>
+    </main>
+    <Footer />
     </>
   );
 }
